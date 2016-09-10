fcada2c1f0e49f6220e0c2e2e645bab06526fb97
Andrew Rivett <veggiefrog@gmail.com>
2016-09-10 12:38:43 -0400

    Added hooks/uiSettings for new business menu

--- hooks/uiSettings.php
+++ hooks/uiSettings.php
@@ -7,23 +7,44 @@
 // Arguments
 // ---------
 // ciniki:
-// business_id:        The ID of the business to get events for.
+// business_id:        The ID of the business to get conferences for.
 //
 // Returns
 // -------
 //
 function ciniki_conferences_hooks_uiSettings($ciniki, $business_id, $args) {
 
-    $settings = array();
+    //
+    // Setup the default response
+    //
+    $rsp = array('stat'=>'ok', 'settings'=>array(), 'menu_items'=>array());
 
     //
     // Get the settings
     //
     $rc = ciniki_core_dbDetailsQueryDash($ciniki, 'ciniki_conferences_settings', 'business_id', $business_id, 'ciniki.conferences', 'settings', '');
     if( $rc['stat'] == 'ok' && isset($rc['settings']) ) {
-        $settings = $rc['settings'];
+        $rsp['settings'] = $rc['settings'];
     }
 
-    return array('stat'=>'ok', 'settings'=>$settings);    
+    //
+    // Check permissions for what menu items should be available
+    //
+    if( isset($ciniki['business']['modules']['ciniki.conferences'])
+        && (isset($args['permissions']['owners'])
+            || isset($args['permissions']['employees'])
+            || isset($args['permissions']['resellers'])
+            || ($ciniki['session']['user']['perms']&0x01) == 0x01
+            )
+        ) {
+        $menu_item = array(
+            'priority'=>6200,
+            'label'=>'Conference Manager', 
+            'edit'=>array('app'=>'ciniki.conferences.main'),
+            );
+        $rsp['menu_items'][] = $menu_item;
+    } 
+
+    return $rsp;
 }
 ?>
